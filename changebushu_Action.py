c8089dbf7da077aea4060e66c89d8f3fb4f27148
luis-arch <61920061+luis-arch@users.noreply.github.com>
2025-02-14 09:34:01 +0800

    Update changebushu_Action.py

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -178,5 +178,5 @@ if __name__ == "__main__":
     # step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
     # step = os.environ['STEP']
     # step = str(randint(5000, 8000)) 
-    step = 8745
+    step = 6574
     main(user,password,step)
