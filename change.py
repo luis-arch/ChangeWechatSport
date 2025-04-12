89f211b720b989adc696c33b80c20ea922cbada8
luis-arch <61920061+luis-arch@users.noreply.github.com>
2025-04-12 08:43:05 +0800

    Update change.py

--- change.py
+++ change.py
@@ -178,5 +178,5 @@ if __name__ == "__main__":
     # step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
     # step = os.environ['STEP']
     # step = str(randint(5000, 8000)) 
-    step = 14143
+    step = 4127
     main(user,password,step)
